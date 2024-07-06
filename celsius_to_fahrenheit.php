6259b30b4a8919c4cd82e3112802a8455b696c59
Md. Rashedul Islam <rashed10cse.hstu@gmail.com>
2024-07-06 20:07:27 +0600

    Add files via upload

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,6 @@
+<?php
+$temperature = 32; // temperature value in Celsius
+$fahrenheit = ($temperature * 9/5) + 32;
+
+printf("The temperature %d degree Celsius in Fahrenheit is %.2f degrees.",$temperature, $fahrenheit);
+?>
